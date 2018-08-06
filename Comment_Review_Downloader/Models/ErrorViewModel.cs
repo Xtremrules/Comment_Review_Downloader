291c3f9cfc4e0da76ecd49db5f7e578eefd22845
Xtremrules <oleleweprince@outlook.com>
2018-08-06 22:07:40 +0100

    Add project files.

---
+++ Comment_Review_Downloader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Comment_Review_Downloader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
