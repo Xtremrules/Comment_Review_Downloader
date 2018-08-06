e63313acdf06b4fb77911ce66820228f78a328b2
Xtremrules <oleleweprince@outlook.com>
2018-08-06 22:29:47 +0100

    Added context to startup

--- Comment_Review_Downloader/Startup.cs
+++ Comment_Review_Downloader/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Comment_Review_Downloader.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,7 @@ namespace Comment_Review_Downloader
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<CommentsDbContext>(options => options.UseSqlite("Data Source = CommentsTable.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
