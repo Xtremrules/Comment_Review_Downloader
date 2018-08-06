f2a17ff8bb846611fe648b034328c00a7164cd78
Xtremrules <oleleweprince@outlook.com>
2018-08-06 22:29:19 +0100

    Added context

---
+++ Comment_Review_Downloader/Data/CommentsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment_Review_Downloader.Data
+{
+    public class CommentsDbContext : DbContext
+    {
+    }
+}
