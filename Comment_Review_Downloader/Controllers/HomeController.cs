da04c7e32a559ff706e807f477a2b0dcd23a5ad4
Xtremrules <oleleweprince@outlook.com>
2018-08-07 06:29:22 +0100

    added requestviewmodel class

--- Comment_Review_Downloader/Controllers/HomeController.cs
+++ Comment_Review_Downloader/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Comment_Review_Downloader.Models;
 
@@ -12,7 +13,32 @@ namespace Comment_Review_Downloader.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(new RequestViewModel());
+        }
+
+        public IActionResult Index(RequestViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.RequestUrl.Contains("amazon"))
+            {
+                return View("amazon-download", model.RequestUrl);
+            }
+            else
+            {
+                var match = "^(http(s)??\\:\\/\\/)?(www\\.|m\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_]{11})$";
+                var isMatch = Regex.IsMatch(model.RequestUrl, match, RegexOptions.IgnoreCase);
+                if (isMatch)
+                {
+                    var youtubeId = model.RequestUrl.Substring(model.RequestUrl.Length - 11);
+                    return View("youtube-download", model.RequestUrl);
+                }
+
+                return View(model);
+            }
         }
 
         public IActionResult About()
---
+++ Comment_Review_Downloader/Models/RequestViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment_Review_Downloader.Models
+{
+    public class RequestViewModel
+    {
+        [Required]
+        public string RequestUrl { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
