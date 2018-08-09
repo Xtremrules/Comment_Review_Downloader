feb7886a1e238a8eba01f8e1596b9da2779349e3
Xtremrules <oleleweprince@outlook.com>
2018-08-09 22:40:14 +0100

    Can view YouTube comments

--- Comment_Review_Downloader/Controllers/HomeController.cs
+++ Comment_Review_Downloader/Controllers/HomeController.cs
@@ -9,20 +9,30 @@ using Comment_Review_Downloader.Models;
 using Comment_Review_Downloader.Data.Interface;
 using Microsoft.Extensions.Logging;
 using Comment_Review_Downloader.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Net;
 
 namespace Comment_Review_Downloader.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly IRepository<Comment> _commentRepo;
+        private readonly IRepository<Data.Entity.Comment> _commentRepo;
         private readonly IRepository<CommentRequest> _commentRequestRepo;
+        private readonly string _endpoint;
+        private readonly string _apiKey;
+        private readonly HttpClient _httpClient;
 
-        public HomeController(IRepository<Comment> comment, IRepository<CommentRequest> commentRequest, ILogger<HomeController> logger)
+        public HomeController(IRepository<Data.Entity.Comment> comment, IRepository<CommentRequest> commentRequest,
+            ILogger<HomeController> logger, IConfiguration config, HttpClient httpClient)
         {
+            _httpClient = httpClient;
             _commentRepo = comment;
             _commentRequestRepo = commentRequest;
             _logger = logger;
+            _apiKey = config["Youtube:ApiKey"];
+            _endpoint = "https://www.googleapis.com/youtube/v3/commentThreads?key=" + _apiKey + "&part=snippet&maxResults=100&videoId={0}";
         }
 
         public IActionResult Index()
@@ -55,15 +65,15 @@ namespace Comment_Review_Downloader.Controllers
                     {
 
                         throw;
-                    } 
+                    }
                 }
                 else
                 {
-                    addError("Not a valid Amazon Product page");
+                    AddError("Not a valid Amazon Product page");
                     return View(model);
                 }
             }
-            else if(model.RequestUrl.Contains("youtube"))
+            else if (model.RequestUrl.Contains("youtube"))
             {
                 var regEx = "^(http(s)??\\:\\/\\/)?(www\\.|m\\.)?((youtube\\.com\\/watch\\?v=)|(youtu.be\\/))([a-zA-Z0-9\\-_]{11})$";
                 var isMatch = Regex.IsMatch(model.RequestUrl, regEx, RegexOptions.IgnoreCase);
@@ -72,23 +82,25 @@ namespace Comment_Review_Downloader.Controllers
                     try
                     {
                         await AddRequesAsync(model);
+                        model.RequestUrl = model.RequestUrl.Substring(model.RequestUrl.Length - 11);
+                        await GetYouTubeCommentAsync(model.RequestUrl);
                         return View("youtube-download", model);
                     }
-                    catch (Exception)
+                    catch (Exception ex)
                     {
-
-                        throw;
+                        AddError(ex.Message);
+                        return View(model);
                     }
                 }
                 else
                 {
-                    addError("Not a Valid YouTube Address");
+                    AddError("Not a Valid YouTube Address");
                     return View(model);
                 }
 
             }
 
-            addError("Not an Amazon or YouTube address");
+            AddError("Not an Amazon or YouTube address");
             return View(model);
         }
 
@@ -108,7 +120,7 @@ namespace Comment_Review_Downloader.Controllers
             }
             else
             {
-                comment = new Comment
+                comment = new Data.Entity.Comment
                 {
                     Url = model.RequestUrl,
                     DateAdded = DateTime.Now,
@@ -116,21 +128,58 @@ namespace Comment_Review_Downloader.Controllers
                     Disabled = false
                 };
 
-                comment.CommentRequests = new List<CommentRequest>();
-                comment.CommentRequests.Add(new CommentRequest
+                comment.CommentRequests = new List<CommentRequest>
                 {
-                    dateRequested = DateTime.Now,
-                    emailAddress = model.Email,
-                    emailed = false,
-                });
+                    new CommentRequest
+                    {
+                        dateRequested = DateTime.Now,
+                        emailAddress = model.Email,
+                        emailed = false,
+                    }
+                };
                 _commentRepo.Create(comment);
                 await _commentRepo.SaveAsync();
             }
         }
 
-        private void addError(string message)
+        private void AddError(string message)
         {
             TempData["error"] = message;
+        }
+
+        private async Task GetYouTubeCommentAsync(string videoId)
+        {
+            var query = string.Format(_endpoint, videoId);
+            var response = await _httpClient.GetAsync(query);
+
+            if (response.StatusCode == HttpStatusCode.Forbidden)
+                return;
+
+            var currentResponse = new YouTubeCommentSet();
+            var refinedComments = new List<RefinedComment>();
+
+            currentResponse = await response.Content.ReadAsAsync<YouTubeCommentSet>();
+            var snippets = currentResponse.Items.Select(e => GetRefinedComment(e, videoId));
+            refinedComments.AddRange(snippets);
+
+            ViewBag.refinedComments = refinedComments;
+        }
+
+        private RefinedComment GetRefinedComment(Comment comment, string videoId)
+        {
+            return new RefinedComment
+            {
+                Username = comment.Snippet.TopLevelComment.Snippet.AuthorDisplayName,
+                Date = comment.Snippet.TopLevelComment.Snippet.PublishedAt.ToLongDateString(),
+                Rating = comment.Snippet.TopLevelComment.Snippet.ViewerRating,
+                Comment = comment.Snippet.TopLevelComment.Snippet.TextOriginal,
+                Link = GetDirectCommentLink(videoId, comment.Snippet.TopLevelComment.Id)
+            };
+        }
+
+        public string GetDirectCommentLink(string videoId, string commentId)
+        {
+            return $"https://www.youtube.com/watch?v={videoId}&lc={commentId}";
         }
 
         public IActionResult About()
@@ -157,5 +206,48 @@ namespace Comment_Review_Downloader.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region Dublication
+
+        private class YouTubeCommentSet
+        {
+            public string NextPageToken { get; set; }
+            public PageInfo PageInfo { get; set; }
+            public IList<Comment> Items { get; set; }
+        }
+
+        private class PageInfo
+        {
+            public int TotalResults { get; set; }
+        }
+
+        private class Comment
+        {
+            public string Id { get; set; }
+            public ParentSnippet Snippet { get; set; }
+
+        }
+
+        private class ParentSnippet
+        {
+            public string VideoId { get; set; }
+            public TopLevelComment TopLevelComment { get; set; }
+        }
+
+        private class TopLevelComment
+        {
+            public string Id { get; set; }
+            public ChildSnippet Snippet { get; set; }
+        }
+
+        private class ChildSnippet
+        {
+            public string AuthorDisplayName { get; set; }
+            public DateTime PublishedAt { get; set; }
+            public string ViewerRating { get; set; }
+            public string TextOriginal { get; set; }
+        }
+
+        #endregion
     }
 }
